202b7155fef5caf0373398306db72c4052d7ba19
yaoxiaohua <6056446@qq.com>
2018-10-24 13:41:32 +0800

    1.4

--- zijiwan/wangyiyun.py
+++ zijiwan/wangyiyun.py
@@ -2,11 +2,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-f=0
-l=0
-d = '%s'%f
-while f < 37:
-    url = 'https://music.163.com/discover/playlist?order=hot&cat=%E6%B0%91%E8%B0%A3&limit=35&offset='+d
+muick = open('muick.txt', 'a',encoding='utf8')
+name = []
+URL = []
+sc = []
+bf = []
+r = 0
+l = 0
+d = 0
+while d < 1225:
+    url = 'https://music.163.com/discover/playlist?order=hot&cat=%E6%B0%91%E8%B0%A3&limit=35&offset='+'%s'%d
+    print(url)
     h = {
         'Host': 'music.163.com',
         'Connection': 'keep-alive',
@@ -22,21 +28,28 @@ while f < 37:
     t = w.text
     soup = BeautifulSoup(t, 'lxml')
     list = soup.select('a.tit')
-
-    while l < len(list):
+    for z1 in list:
+        name.append(z1.string)
+        muick.writelines(z1.string)
+        muick.writelines('\n')
+    for l in range(len(list)):
         url2 = 'https://music.163.com'+list[l]['href']
         w2 = s.get(url2)
-        print(url2)
+        URL.append(url2)
+        muick.writelines(url2)
+        muick.writelines('\n')
         t2 = w2.text
         soup2 = BeautifulSoup(t2, 'lxml')
         list2 = soup2.select('a.u-btni-fav i')
-        print('收藏：', list2[0].string)
+        sc.append('收藏：' )
+        muick.writelines('收藏：' )
+        sc.append(list2[0].string)
+        muick.writelines(list2[0].string)
+        muick.writelines('\n')
         list3 = soup2.select('div.more')
-        print(soup2.select('div.more')[0].get_text())
+        bf.append(soup2.select('div.more')[0].get_text())
+        muick.writelines(soup2.select('div.more')[0].get_text())
+        muick.writelines('\n')
+        print('第%s首'%l)
         l+=1
-    f+=1
-
-p = 0
-while p < len(list):
-    print(soup.select('a.tit')[p].string)
-    p += 1
+    d+=35
